3b64e67f28c18cc5536010b3d3df7bce91d16657
Your Name <you@example.com>
2021-09-30 14:26:54 -0600

    files

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from users_app import app
-from users_app import users_controller
+from users_app.controllers import users_controller
 
 
 
